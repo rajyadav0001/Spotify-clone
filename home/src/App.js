832fb910363dbcd17a0386f3ab4fc70e600e1207
Himanshu Mishra <himanshumishra389@gmai.com>
2023-02-10 17:15:44 +0530

    friday work

--- home/src/App.js
+++ home/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Login from "./Login";
+import React, { useEffect } from "react";
+import './App.css'
+import { getTokenFronUrl } from "./spotify";
 
 function App() {
+   /* {Run code based on a given condition} */
+   useEffect(()=>{
+    const token=getTokenFronUrl();
+    window.location.hash="";
+    console.log('I HAVE A TOKEN',token);
+
+   },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+  
+      <Login />
+     
     </div>
   );
 }
