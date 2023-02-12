30a4adc7e4ea18f9a84dacd4ad3366d5e51d011b
rajyadav0001 <rajneshsbl9536@gmail.com>
2023-02-12 13:35:41 +0530

    Upadted folder structure

--- spotify-clone-in-react/src/App.js
+++ spotify-clone-in-react/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Login from "./Login";
+import React, { useEffect, useState } from "react";
+import './App.css'
+import { getTokenFronUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js"
+
+
+
+const spotify =new SpotifyWebApi();
+
 
 function App() {
+  const[token,setToken]=useState(null);
+   /* {Run code based on a given condition} */
+   useEffect(()=>{
+    const hash=getTokenFronUrl();
+    window.location.hash="";
+    const _token=hash.access_token;
+    console.log('I HAVE A TOKEN',token);
+    
+
+    if(_token){
+      setToken(_token)
+    }
+
+   },[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">{token ? <h1>I am logged in</h1> : <Login />}</div>
   );
 }
 
